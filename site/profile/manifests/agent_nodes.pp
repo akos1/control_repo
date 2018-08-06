e1a002ad2db65b0980525e9b1a6335dd65d3d43e
akos1 <zeleiakos1@gmail.com>
2018-08-06 16:26:25 +0200

    Update agent_nodes.pp

--- site/profile/manifests/agent_nodes.pp
+++ site/profile/manifests/agent_nodes.pp
@@ -1,5 +1,12 @@
 class profile::agent_nodes {
-  include dockeragent
-  dockeragent::node { 'web.puppet.vm': }
-  dockeragent::node { 'db.puppet.vm': }
+  
+  host {'web.puppet.vm':
+  ensure => present,
+  ip      => '192.168.1.2'
+  }
+  
+  host {'db.puppet.vm':
+  ensure => present,
+  ip      => '192.168.1.3'
+  }
 }
